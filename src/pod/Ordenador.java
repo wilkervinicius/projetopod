5b3d51b9cbd9231f4ad7313142e18fb17510ad5c
Wilker Vinicius <wilkervinicius@gmail.com>
2015-09-08 03:24:20 -0300

    melhoria no codigo insertion(troca) e implementacao do shell

--- src/pod/Ordenador.java
+++ src/pod/Ordenador.java
@@ -45,13 +45,13 @@ public class Ordenador {
 
         int i, a;
 
-        for (i = 0; i < (vetor.length - 2); i++) {
+        for (i = 0; i < (vetor.length -1 ); i++) {
             a = i;
-            int j = i + 1;
-            while ((j < vetor.length - 1) && (vetor[a] > vetor[j])) {
-
-                a = j;
-                j++;
+            int j ;
+            for (j = a + 1;(j < vetor.length) ;j++) {
+                 if (vetor[a] > vetor[j])
+                  a = j;
+                
 
             }
 
@@ -66,18 +66,21 @@ public class Ordenador {
         return vetor;
     }
 
+    
     public int[] insercao(int[] vetor) {
 
         int i, j;
 
-        for (i = 1; i < vetor.length; i++) {
+        for (i = 0; i < vetor.length; i++) {
 
             j = i;
-            while (j > 0 && vetor[j - 1] > vetor[j]) {
+            
+            if (j>=vetor.length) 
+            	 break;
+            
+            while (j > 0 &&  vetor[j] > vetor[j + 1]) {
 
-               int aux = vetor[j-1];
-               vetor[j-1] = vetor[j];
-               vetor[j] = aux;
+                troca(vetor, j);
                 j--;
             }
 
@@ -87,6 +90,43 @@ public class Ordenador {
 
     }
 
+    
+    //ordenação concha ou shell sort
+    public int [] concha( int [] vetor) {
+    	
+    	int salto,i,j,k;
+    	salto = vetor.length/2;
+    	
+    	while (salto>0) {
+    		for (i = salto; i< vetor.length; i++) {
+    			j = i - salto;
+    			while (j>=0){
+    				
+    				k = j + salto;
+    				if (vetor[j]<=vetor[k]){
+    					j=-1;
+    					
+    				} else {
+    					
+    					troca(vetor,j);
+    					j-=salto; // j = j-salto
+    				}
+    			}
+    		}
+    		
+    		salto/=2;
+    	}
+    	
+    	
+    	
+    	return vetor;
+    }
+    
+    
+    
+    
+    
+    
     public int[] troca(int[] vetor, int j) {
 
         int aux = vetor[j];
